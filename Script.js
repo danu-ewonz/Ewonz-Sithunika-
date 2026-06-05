f9b0ca20317ee584bf71cd5c7afc141ce472745b
danu-ewonz <ewonz2006@gmail.com>
2026-06-05 20:58:38 +0530

    Refactor countdown functions and formatting

--- Script.js
+++ Script.js
@@ -1,5 +1,4 @@
-/* OPEN BUTTON */
-function openInvitation() {
+function openInvitation(){
   document.getElementById("openScreen").style.display = "none";
   document.getElementById("weddingContent").style.display = "block";
   updateCountdown();
@@ -9,11 +8,11 @@ function openInvitation() {
 const countdownEl = document.getElementById("countdown");
 const weddingDate = new Date("2026-11-28T19:00:00+05:30").getTime();
 
-function updateCountdown() {
+function updateCountdown(){
   const now = new Date().getTime();
   const distance = weddingDate - now;
 
-  if (distance <= 0) {
+  if(distance <= 0){
     countdownEl.innerHTML = "We Are Married ❤️";
     return;
   }
@@ -24,8 +23,7 @@ function updateCountdown() {
   const s = Math.floor((distance%(1000*60))/1000);
 
   countdownEl.innerHTML =
-    d + " Days " + h + " Hours " + m + " Minutes " + s + " Seconds";
+    `${d} Days ${h} Hours ${m} Minutes ${s} Seconds`;
 }
 
-/* RUN TIMER */
-setInterval(updateCountdown, 1000);
+setInterval(updateCountdown,1000);
